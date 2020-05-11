a93e2e2ef0761b31aa0bd1e7f74ab0c85008ccdb
Jnorman2123 <Jnorman2123@gmail.com>
2020-05-11 13:13:03 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = function () {
+    return {
+        characters: [
+            { 
+                id: 1,
+                name: 'Kirill',
+                class: 'Shaman',
+                race: 'Barbarian',
+                level: '11'  
+            },
+            {   
+                id: 2,
+                name: 'Merill',
+                class: 'Ranger',
+                race: 'Half-elf',
+                level: '3'
+            }],
+            equipment: [
+                {
+                    id: 1,
+                    slot: '',
+                }
+            ]
+    }
+}
